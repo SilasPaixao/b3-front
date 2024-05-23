5049fdb521e21601a99710d68cef439897f9321e
Silas Paixao <silas.paixao873@gmail.com>
2024-05-23 17:39:51 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -18,8 +18,8 @@ const Login: React.FC = () => {
       <Context.Provider value={ { state, errorState }}>
         <form className={Styles.form}>
           <h2>Login</h2>
-          <Input type="email" name={'email'} placeholder="Digite seu e-mail" />
-          <Input type="password" name={'password'} placeholder="Digite sua senha" />
+          <Input type="email" name="email" placeholder="Digite seu e-mail" />
+          <Input type="password" name="password" placeholder="Digite sua senha" />
           <button data-testid="submit" disabled className={Styles.submit} type="submit">Entrar</button>
           <span className={Styles.link}>Criar conta</span>
           <FormStatus />
